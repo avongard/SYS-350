8c07e4e2ab9cf43ac8636ecdc2c1bc0bc9155f9c
avon <95338964+avongard@users.noreply.github.com>
2023-10-19 19:44:51 -0400

    Add files via upload

--- vm_data.py
+++ vm_data.py
@@ -1,8 +1,10 @@
 import getpass
+import pdb
 
 passw= getpass.getpass()
 
 from pyVim.connect import SmartConnect
+from pyVmomi import vim
 
 import ssl
 
@@ -14,22 +16,23 @@ s.verify_mode=ssl.CERT_NONE
 si=SmartConnect(host=hostname, user=username, pwd=passw, sslContext=s)
 content=si.RetrieveContent()
 
-choice = input("What VM would you like to search for?:")
 datacenter = si.content.rootFolder.childEntity[0] # Root folder of server and all inventory attached
 vms = datacenter.vmFolder.childEntity # Folder in particular to look at
-machine = []
-vm_name = ManagedEntity[1]
+vm_view = content.viewManager.CreateContainerView(content.rootFolder, [vim.VirtualMachine], True)
+machine = vm_view.view
+vm_info = {}
 
-for i in vms: # Loop through list of VM's
-    machine.append(i.name)
-    
-if choice in machine:
-    print("VM Name: " + choice)
-    print("Power State: " + vms.vm_name.runtime.powerState)
-    print("CPUs: " )
-    print("Memory: " + " GB")
-    print("IP Address: " )
-elif choice == "":
-    print(machine)
-else:
-    print(choice + " does not exist.")
+for vm in machine:
+    if vm.summary.guest is not None:
+        ip_address = vm.summary.guest.ipAddress
+        power_state = vm.summary.runtime.powerState
+        cpu = vm.summary.config.numCpu
+        memory_mb = vm.summary.config.memorySizeMB
+        memory_gb = memory_mb * .001
+        vm_info[vm.name] = ip_address
+        print("Name: " + vm.name)
+        print("IP: " + str(ip_address))
+        print("Power State: " + str(power_state))
+        print("CPUs: " + str(cpu))
+        print("Memory: " + str(memory_gb) + " GB")
+        print("")
