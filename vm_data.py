d94ab31300c4f612759578141311506c32b13120
avongard <avongardavongard@gmail.com>
2023-10-19 19:37:02 -0400

    PyvMomi pt 1 Complete

--- vm_data.py
+++ vm_data.py
@@ -14,16 +14,21 @@ s.verify_mode=ssl.CERT_NONE
 si=SmartConnect(host=hostname, user=username, pwd=passw, sslContext=s)
 content=si.RetrieveContent()
 
+choice = input("What VM would you like to search for?:")
 datacenter = si.content.rootFolder.childEntity[0] # Root folder of server and all inventory attached
 vms = datacenter.vmFolder.childEntity # Folder in particular to look at
-choice = input("What VM would you like to search for?:")
 machine = []
+vm_name = ManagedEntity[1]
 
 for i in vms: # Loop through list of VM's
     machine.append(i.name)
     
 if choice in machine:
-    print(choice + " exists")
+    print("VM Name: " + choice)
+    print("Power State: " + vms.vm_name.runtime.powerState)
+    print("CPUs: " )
+    print("Memory: " + " GB")
+    print("IP Address: " )
 elif choice == "":
     print(machine)
 else:
